6756bbf47c87147620db2d0b19a4bb6b716dd41d
Stephen Becker IV <github@deathbyescalator.com>
2018-10-31 08:23:54 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ while sleep 60; do
   # If they are not both 0, then something is wrong
   if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
-    exit 1
+    # exit 1
   fi
 done
 
